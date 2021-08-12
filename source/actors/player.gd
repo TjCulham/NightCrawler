62f04e4f07b17c522776128d23446a97d3e57605
Thomas-Culham <tj.culham@gmail.com>
2021-08-12 18:34:46 +1200

    Player movement
    added player movement, jump, left right

--- source/actors/player.gd
+++ source/actors/player.gd
@@ -1,3 +1,24 @@
 extends Actor
 
+func _physics_process(delta: float) -> void:
+	var direction: = getDirection()
+	velocity = calculateMoveVelocity(velocity, direction, speed)
+	velocity = move_and_slide(velocity, FLOOR_NORMAL)
 
+func getDirection() -> Vector2:
+	return Vector2(
+		Input.get_action_strength("moveRight") - Input.get_action_strength("moveLeft"),
+		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
+	)
+	
+func calculateMoveVelocity(
+		linearVelocity: Vector2,
+		direction: Vector2,
+		speed: Vector2
+	) -> Vector2:
+	var out: = linearVelocity
+	out.x = speed.x * direction.x
+	out.y += gravity * get_physics_process_delta_time()
+	if direction.y == -1.0:
+		out.y = speed.y * direction.y
+	return out
